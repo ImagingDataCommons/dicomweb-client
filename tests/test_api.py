6d857ce456680b63451ce284ab94cbd60b7a21af
hackermd <hackermd@protonmail.com>
2020-02-07 17:23:19 -0500

    Fix problems with os.path in tests for python 3.5

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,3 @@
-import os
 import json
 import xml.etree.ElementTree as ET
 from io import BytesIO
@@ -80,7 +79,7 @@ def test_lookup_keyword(httpserver, client):
 
 
 def test_search_for_studies(httpserver, client, cache_dir):
-    cache_filename = os.path.join(cache_dir, 'search_for_studies.json')
+    cache_filename = cache_dir.joinpath('search_for_studies.json')
     with open(cache_filename, 'r') as f:
         content = f.read()
     parsed_content = json.loads(content)
@@ -97,7 +96,7 @@ def test_search_for_studies(httpserver, client, cache_dir):
 
 def test_search_for_studies_qido_prefix(httpserver, client, cache_dir):
     client.qido_url_prefix = 'qidors'
-    cache_filename = os.path.join(cache_dir, 'search_for_studies.json')
+    cache_filename = cache_dir.joinpath('search_for_studies.json')
     with open(cache_filename, 'r') as f:
         content = f.read()
     headers = {'content-type': 'application/dicom+json'}
@@ -108,7 +107,7 @@ def test_search_for_studies_qido_prefix(httpserver, client, cache_dir):
 
 
 def test_search_for_studies_limit_offset(httpserver, client, cache_dir):
-    cache_filename = os.path.join(cache_dir, 'search_for_studies.json')
+    cache_filename = cache_dir.joinpath('search_for_studies.json')
     with open(cache_filename, 'r') as f:
         data = json.loads(f.read())
     # We will limit the search to 2 studies starting with the 2nd.
@@ -130,7 +129,7 @@ def test_search_for_studies_limit_offset(httpserver, client, cache_dir):
 
 
 def test_search_for_series(httpserver, client, cache_dir):
-    cache_filename = os.path.join(cache_dir, 'search_for_series.json')
+    cache_filename = cache_dir.joinpath('search_for_series.json')
     with open(cache_filename, 'r') as f:
         content = f.read()
     parsed_content = json.loads(content)
@@ -158,7 +157,7 @@ def test_search_for_series_wrong_uid_value(httpserver, client, cache_dir):
 
 
 def test_search_for_series_limit_offset(httpserver, client, cache_dir):
-    cache_filename = os.path.join(cache_dir, 'search_for_series.json')
+    cache_filename = cache_dir.joinpath('search_for_series.json')
     with open(cache_filename, 'r') as f:
         data = json.loads(f.read())
     content = json.dumps(data[1:3])
@@ -179,7 +178,7 @@ def test_search_for_series_limit_offset(httpserver, client, cache_dir):
 
 
 def test_search_for_instances(httpserver, client, cache_dir):
-    cache_filename = os.path.join(cache_dir, 'search_for_instances.json')
+    cache_filename = cache_dir.joinpath('search_for_instances.json')
     with open(cache_filename, 'r') as f:
         content = f.read()
     parsed_content = json.loads(content)
@@ -195,7 +194,7 @@ def test_search_for_instances(httpserver, client, cache_dir):
 
 
 def test_search_for_instances_limit_offset(httpserver, client, cache_dir):
-    cache_filename = os.path.join(cache_dir, 'search_for_instances.json')
+    cache_filename = cache_dir.joinpath('search_for_instances.json')
     with open(cache_filename, 'r') as f:
         content = f.read()
     parsed_content = json.loads(content)
@@ -235,7 +234,7 @@ def test_search_for_instances_includefields(httpserver, client, cache_dir):
 
 
 def test_retrieve_instance_metadata(httpserver, client, cache_dir):
-    cache_filename = os.path.join(cache_dir, 'retrieve_instance_metadata.json')
+    cache_filename = cache_dir.joinpath('retrieve_instance_metadata.json')
     with open(cache_filename, 'r') as f:
         content = f.read()
     parsed_content = json.loads(content)
@@ -262,7 +261,7 @@ def test_retrieve_instance_metadata(httpserver, client, cache_dir):
 
 def test_retrieve_instance_metadata_wado_prefix(httpserver, client, cache_dir):
     client.wado_url_prefix = 'wadors'
-    cache_filename = os.path.join(cache_dir, 'retrieve_instance_metadata.json')
+    cache_filename = cache_dir.joinpath('retrieve_instance_metadata.json')
     with open(cache_filename, 'r') as f:
         content = f.read()
     parsed_content = json.loads(content)
@@ -284,7 +283,7 @@ def test_retrieve_instance_metadata_wado_prefix(httpserver, client, cache_dir):
 
 
 def test_retrieve_instance(httpserver, client, cache_dir):
-    cache_filename = os.path.join(cache_dir, 'file.dcm')
+    cache_filename = cache_dir.joinpath('file.dcm')
     with open(cache_filename, 'rb') as f:
         content = f.read()
     headers = {
@@ -314,7 +313,7 @@ def test_retrieve_instance(httpserver, client, cache_dir):
 
 
 def test_retrieve_instance_any_transfer_syntax(httpserver, client, cache_dir):
-    cache_filename = os.path.join(cache_dir, 'file.dcm')
+    cache_filename = cache_dir.joinpath('file.dcm')
     with open(cache_filename, 'rb') as f:
         content = f.read()
     headers = {
@@ -336,7 +335,7 @@ def test_retrieve_instance_any_transfer_syntax(httpserver, client, cache_dir):
 
 def test_retrieve_instance_default_transfer_syntax(httpserver, client,
                                                    cache_dir):
-    cache_filename = os.path.join(cache_dir, 'file.dcm')
+    cache_filename = cache_dir.joinpath('file.dcm')
     with open(cache_filename, 'rb') as f:
         content = f.read()
     headers = {
@@ -357,7 +356,7 @@ def test_retrieve_instance_default_transfer_syntax(httpserver, client,
 
 
 def test_retrieve_instance_wrong_transfer_syntax(httpserver, client, cache_dir):
-    cache_filename = os.path.join(cache_dir, 'file.dcm')
+    cache_filename = cache_dir.joinpath('file.dcm')
     with open(cache_filename, 'rb') as f:
         content = f.read()
     headers = {
@@ -377,7 +376,7 @@ def test_retrieve_instance_wrong_transfer_syntax(httpserver, client, cache_dir):
 
 
 def test_retrieve_instance_wrong_mime_type(httpserver, client, cache_dir):
-    cache_filename = os.path.join(cache_dir, 'file.dcm')
+    cache_filename = cache_dir.joinpath('file.dcm')
     with open(cache_filename, 'rb') as f:
         content = f.read()
     headers = {
@@ -397,7 +396,7 @@ def test_retrieve_instance_wrong_mime_type(httpserver, client, cache_dir):
 
 
 def test_retrieve_instance_frames_jpeg(httpserver, client, cache_dir):
-    cache_filename = os.path.join(cache_dir, 'retrieve_instance_pixeldata.jpg')
+    cache_filename = cache_dir.joinpath('retrieve_instance_pixeldata.jpg')
     with open(cache_filename, 'rb') as f:
         content = f.read()
     headers = {
@@ -426,7 +425,7 @@ def test_retrieve_instance_frames_jpeg(httpserver, client, cache_dir):
 def test_retrieve_instance_frames_jpeg_default_transfer_syntax(httpserver,
                                                                client,
                                                                cache_dir):
-    cache_filename = os.path.join(cache_dir, 'retrieve_instance_pixeldata.jpg')
+    cache_filename = cache_dir.joinpath('retrieve_instance_pixeldata.jpg')
     with open(cache_filename, 'rb') as f:
         content = f.read()
     headers = {
@@ -448,7 +447,7 @@ def test_retrieve_instance_frames_jpeg_default_transfer_syntax(httpserver,
 
 
 def test_retrieve_instance_frames_jp2(httpserver, client, cache_dir):
-    cache_filename = os.path.join(cache_dir, 'retrieve_instance_pixeldata.jp2')
+    cache_filename = cache_dir.joinpath('retrieve_instance_pixeldata.jp2')
     with open(cache_filename, 'rb') as f:
         content = f.read()
     headers = {
@@ -475,7 +474,7 @@ def test_retrieve_instance_frames_jp2(httpserver, client, cache_dir):
 
 
 def test_retrieve_instance_frames_rendered_jpeg(httpserver, client, cache_dir):
-    cache_filename = os.path.join(cache_dir, 'retrieve_instance_pixeldata.jpg')
+    cache_filename = cache_dir.joinpath('retrieve_instance_pixeldata.jpg')
     with open(cache_filename, 'rb') as f:
         content = f.read()
     headers = {
@@ -521,7 +520,7 @@ def test_retrieve_instance_frames_rendered_jpeg_transfer_syntax(httpserver,
 
 
 def test_retrieve_instance_frames_rendered_png(httpserver, client, cache_dir):
-    cache_filename = os.path.join(cache_dir, 'retrieve_instance_pixeldata.png')
+    cache_filename = cache_dir.joinpath('retrieve_instance_pixeldata.png')
     with open(cache_filename, 'rb') as f:
         content = f.read()
     headers = {
@@ -627,7 +626,7 @@ def test_load_json_dataset_pn_vm2_empty(httpserver, client, cache_dir):
 
 
 def test_load_xml_response(httpserver, client, cache_dir):
-    cache_filename = os.path.join(cache_dir, 'store.xml')
+    cache_filename = cache_dir.joinpath('store.xml')
     with open(cache_filename, 'rb') as f:
         tree = ET.fromstring(f.read())
         dataset = _load_xml_dataset(tree)
