da747e8573a726dc02b63482ba65ef5cb9893978
babaksamari <babaksamari@google.com>
2020-03-10 13:14:06 -0400

    Implementing session_utils.py and moving all authentication related code from api.py to session_utils.py.

--- src/dicomweb_client/session_utils.py
+++ src/dicomweb_client/session_utils.py
@@ -109,7 +109,8 @@ def create_session_from_gcp_credentials(
         from google.auth.transport import requests as google_requests
         if google_credentials is None:
             import google.auth
-            google_credentials, _ = google.auth.default()
+            google_credentials, _ = google.auth.default(
+                scopes=['https://www.googleapis.com/auth/cloud-platform'])
     except ImportError:
         raise ImportError(
             'The dicomweb-client package needs to be installed with the '
@@ -117,7 +118,4 @@ def create_session_from_gcp_credentials(
             'Google Cloud Healthcare API: pip install dicomweb-client[gcp]'
         )
     logger.debug('initialize Google AuthorizedSession')
-    if google_credentials.scopes is None:
-        scopes = ['https://www.googleapis.com/auth/cloud-platform', ]
-        google_credentials = google_credentials.with_scopes(scopes)
     return google_requests.AuthorizedSession(google_credentials)
