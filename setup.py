3c92986b62a4b7c3f30538be7394846f55d18107
hackermd <hackermd@protonmail.com>
2021-12-14 12:29:27 -0500

    Add newer python versions to setup.py

--- setup.py
+++ setup.py
@@ -33,6 +33,8 @@ setuptools.setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Development Status :: 4 - Beta',
     ],
     entry_points={
