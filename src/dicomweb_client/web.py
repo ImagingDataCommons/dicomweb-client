5cf9a4bebaf59532deae12ef7a3392be48a5eb76
Dimitris Rozakis <dimrozakis@gmail.com>
2022-11-13 18:28:50 +0200

    Optimize _encode_multipart_message
    By using BytesIO while generating the body instead of continuous string
    concatenations.
    
    This drastically reduces execution time by not recreating the string for
    each concatenation.
    
    In one case, execution time fell from 140 seconds to 0.14; three orders
    of magnitude!

--- src/dicomweb_client/web.py
+++ src/dicomweb_client/web.py
@@ -743,13 +743,15 @@ class DICOMwebClient:
             raise ValueError(
                 'No "boundary" parameter in found in content-type field'
             )
-        body = b''
-        for part in content:
-            body += f'\r\n--{boundary}'.encode('utf-8')
-            body += f'\r\nContent-Type: {content_type}\r\n\r\n'.encode('utf-8')
-            body += part
-        body += f'\r\n--{boundary}--'.encode('utf-8')
-        return body
+        with BytesIO() as b:
+            for part in content:
+                b.write(f'\r\n--{boundary}'.encode('utf-8'))
+                b.write(
+                    f'\r\nContent-Type: {content_type}\r\n\r\n'.encode('utf-8')
+                )
+                b.write(part)
+            b.write(f'\r\n--{boundary}--'.encode('utf-8'))
+            return b.getvalue()
 
     @classmethod
     def _assert_media_type_is_valid(cls, media_type: str):
